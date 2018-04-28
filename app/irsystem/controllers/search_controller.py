11470d1a6c1163aaaf8312836c5bb57da53ee36a
Jerica Huang <jerica.huang@gmail.com>
2018-04-28 15:58:33 -0400

    add updated search

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -6,6 +6,9 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 import sys
 import pickle
+import scipy
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -15,9 +18,12 @@ net_id = "Amanda Chen (aec255), Pegah Moradi (pm443), Nina Ray (nr327), Jerica H
 top_5_dict_words = pickle.load( open( "top_5_dict_words.pickle", "rb" ) )
 top_5_dict_women = pickle.load( open( "top_5_dict_women.pickle", "rb" ) )
 women_name_to_data = pickle.load( open( "women_name_to_data.pickle", "rb" ) )
+deduped_women = pickle.load( open( "deduped_women.pickle", "rb" ) )
+pkl_file = open('myvectorizer.pickle', 'rb')
+vectorizer = pickle.load(pkl_file)
+matx = pickle.load(pkl_file)
 
 @irsystem.route('/', methods=['GET'])
-
 
 def search():
 	query = request.args.get('search')
@@ -26,11 +32,9 @@ def search():
 		output_message = ''
 	else:
 		output_message = "You searched for a woman who " + query
-		
-		if query in top_5_dict_words:
-			data = top_5_dict_words[query]
-			
-		elif "is similar to " in query:
+		q_vec = vectorizer.transform([query])
+
+		if "is similar to " in query:
 			woman = query.split("is similar to ")[1]
 			woman = woman.title()
 			if woman in top_5_dict_women:
@@ -46,39 +50,27 @@ def search():
 			else:
 				data = ["No results :("]
 				
-		elif "is an " in query:
-			word = query.split("is an ")[1]
-			if word in top_5_dict_words:
-				data = top_5_dict_words[word]
-			else:
-				data = ["No results :("]
-		elif "is a " in query:
-			word = query.split("is a ")[1]
-			if word in top_5_dict_words:
-				data = top_5_dict_words[word]
-			else:
-				data = ["No results :("]
-				
-		elif "is " in query:
-			woman = query.split("is ")[1]
-			if woman in top_5_dict_women:
-				data = top_5_dict_women[woman]
-			elif woman in top_5_dict_words:
-				data = top_5_dict_words[woman]
-			elif woman.title() in top_5_dict_women:
-				data = [woman.title()]
-				data = data + top_5_dict_women[woman.title()]
-			else:
-				data = ["No results :("]
-				
 		else:
-			data = ["No results :("]
-   
+			sim_doc_scores = cosine_similarity(q_vec, matx)
+			sim_docs = np.argsort(sim_doc_scores.flatten())[::-1]
+			data = []
+			for hit in sim_docs[0:30]:
+				data.append(deduped_women[hit])
+			if len(data)==0:	
+				data = ["No results :("]
+	
 	if data != ["No results :("] and len(data)>0 and type(data[0]) is not dict:
 		for i in range(len(data)):
 			womanname=data[i]
 			data[i] = {"name": womanname, "summary": women_name_to_data[womanname]["summary"], "views": women_name_to_data[womanname]["views"]}
 			if womanname in top_5_dict_women:
 				data[i]["similar"] = top_5_dict_women[womanname]
-				
+	elif len(data)>0 and "views" not in data[0] and "similar" not in data[0]:
+		for i in range(len(data)):
+			womanname=data[i]["name"]
+			data[i]["views"] = women_name_to_data[womanname]["views"]
+			data[i]["similar"] = top_5_dict_women[womanname]
+
+	print(data)
+
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
