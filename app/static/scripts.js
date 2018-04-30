bc7f896bd8c49ed92af6ad89026ea3a0ac6d8cb3
swaggie <amandac188@gmail.com>
2018-04-30 00:56:23 -0400

    styling

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,3 +1,13 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
+
+    function convertName(name) {
+        var replaced = name.split(' ').join('+');
+        return replaced;
+    }
+
+    $("button").click(function () {
+        $("#bottomcorner").fadeOut();
+    });
+
 });
