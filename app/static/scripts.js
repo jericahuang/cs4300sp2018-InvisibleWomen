d56af3d606a4a5a2d728567947180148ed6b5e82
swaggie <amandac188@gmail.com>
2018-04-30 02:25:41 -0400

    random suggested queries

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -11,4 +11,10 @@ $(document).ready(function () {
     });
 
     $('.tip').tipr();
+
+    var list = ["e.g. worked at NASA", "e.g. found a cure", "e.g. is a Cornell professor", "e.g. researched cancer", "e.g. won the Nobel Prize", "e.g. makes video games", "e.g. is a French physicist"],
+        r = Math.floor(Math.random() * list.length);
+    $('#input').prop('placeholder', list[r]);
+
+    
 });
