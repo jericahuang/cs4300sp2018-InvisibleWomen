6deecc82179444f6855f2def52c6de8e929ff5b6
Jerica Huang <jerica.huang@gmail.com>
2018-05-01 02:17:38 -0400

    adding our names

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -6,10 +6,6 @@ $(document).ready(function () {
         return replaced;
     }
 
-    $("button").click(function () {
-        $("#bottomcorner").fadeOut();
-    });
-
     $('.tip').tipr();
 
     var list = ["e.g. researches natural language processing", "e.g. researched plant breeding", "e.g. is a pioneer", "e.g. worked at NASA", "e.g. is a Cornell professor", "e.g. researched cancer", "e.g. won the Nobel Prize", "e.g. makes video games", "e.g. is a French physicist", "e.g. is a lesbian", "e.g. invented the compiler", "e.g. worked during World War II", "e.g. is an American computer scientist", "e.g. is an entrepreneur", "e.g. was born in New York City", "e.g. does research on group theory", "e.g. worked at IBM", "e.g. was an inventor"],
